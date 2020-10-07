3733b4fb57fec73a66332a87efdd1d0c44d75eb7
DannaLuisa <danna12.garo@gmail.com>
2020-10-07 16:09:49 -0500

    'Init'

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,29 @@
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements AfterViewInit {
+  private map;
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 18.847553, -97.098613 ],
+      zoom: 12
+    });
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+    
+    tiles.addTo(this.map);
+  }
+}
